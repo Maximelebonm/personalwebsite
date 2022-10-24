269e7aa2308fa5a22b20dd4df197120b72eb748b
BemMusicianApp <lebon.maxime.m@gmail.com>
2022-10-24 09:57:44 +0200

    maj24102022 git commit -m maj24102022

--- src/screens/homescreen.jsx
+++ src/screens/homescreen.jsx
@@ -10,12 +10,9 @@ const HomeScreen=()=>{
             <div class="flex flex-col items-center z-10">
             <img class="rounded-full w-40 h-40" src={process.env.PUBLIC_URL + '/img/max.jpeg'}/>
             <div class="pt-4 w-96">
-            Pour commencer, une rapide présentation de moi, je m'appelle Maxime, j'ai 31 ans,
+            Pour commencer, une rapide présentation de moi, je m'appelle Maxime,
             j'ai terminé une reconversion professionnelle avec une formation Concepteur Développeur d'Application à l'AFPA
             et souhaite trouver mon 1er emploi en tant que développeur junior.
-            </div>
-            <div class="">
-                Mes soft compétences
             </div>
             </div>
         </div>
--- src/screens/loisirScreen.jsx
+++ src/screens/loisirScreen.jsx
@@ -11,8 +11,8 @@ const LoisirScreen=()=>{
             </div>
             <Undertitle/>
             <div class="pt-4 grid lg:grid-cols-3 sm:grid-cols-2 justify-items-center">
-                    <Card title="Maks Nobel" id="maksnobel" src={process.env.PUBLIC_URL + '/img/maksnobel.png'} desc="Projet musical dont je suis le musicien, ingénieur du son, monteur vidéo, communicant"/>
-                    <Card title="BeMusician" src={process.env.PUBLIC_URL + '/img/bemusician.png'} desc="Projet d'application pour apprendre à jouer d'un instrument"/>
+                    <Card title="Maks Nobel" id="maksnobel" src={process.env.PUBLIC_URL + '/img/maksnobel.png'} desc="Projet musical dont je suis le musicien, ingénieur du son, monteur vidéo, communicant" lien="https://www.youtube.com/channel/UCVFn1CHWxddOMmr1p8-UFJA" textLien="Découvrir mon projet musical" cssLien="p-2 mt-2 text-white font-bold bg-sky-900 hover:bg-sky-600 hover:text-white transition diration-700"/>
+                    <Card title="BeMusician" src={process.env.PUBLIC_URL + '/img/bemusician.png'} desc="Projet d'application pour apprendre à jouer d'un instrument (en cours de développement)" lien="http://bemusician.fr" textLien="découvrir Mon projet d'application"cssLien="p-2 mt-2 text-white font-bold bg-sky-900 hover:bg-sky-600 hover:text-white transition diration-700"/>
             </div>
         </div>
     )
